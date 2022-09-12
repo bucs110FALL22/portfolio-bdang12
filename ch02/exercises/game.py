c873d3fbaf83e4529b82b4a446cf616d688d68a0
bdang2 <112588812+bdang12@users.noreply.github.com>
2022-09-12 21:55:13 +0000

    ch02/exercises: drawing with turtle

--- ch02/exercises/game.py
+++
@@ -1,9 +0,0 @@
-import pygame
-pygame.init()
-screen = pygame.display.set_mode()
-screen.fill(250,0,0)
-pygame.display.flip()
-screen.fill(0,250,0)
-pygame.display.flip()
-screen.fill(0,0,250)
-pygame.display.flip()
