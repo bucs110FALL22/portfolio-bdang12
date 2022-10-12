9804275f69e60965512dfa672eb4d321de82908b
bdang2 <112588812+bdang12@users.noreply.github.com>
2022-10-12 23:54:30 +0000

    ch05/exercises:drawing.py

---
+++ ch05/exercises/drawing.py
@@ -0,0 +1,13 @@
+import turtle
+def drawEqShape(turtle,num_side, len_side):
+  for i in range(num_side):
+    bill.forward(len_side)
+    bill.left(360/num_side)
+
+bill=turtle.Turtle()
+bill.shape("turtle")
+bill.color("green")
+num_side=int(input("Enter the num of sides"))
+len_side=int(input("Enter the side lenght"))
+drawEqShape(bill,num_side,len_side)
+turtle.exitonclick()
--- ch05/exercises/patterns.py
+++ ch05/exercises/patterns.py
@@ -4,5 +4,23 @@ for i in range(a):
 for j in range(a):
     print("*"*(a-j))
     
+#Second solutions
+def star_pyramid():
+  levels = int(input("Enter the desired pyramid height: "))
     
+  for i in range(1, levels + 1):
+    print("*" * i)
 
+#define a function
+def rstar_pyramid():
+  levels = int(input("Enter the desired pyramid height: "))
+  for i in range(levels, 0, -1):
+    print("*" * i)
+levels = int(input("Enter the desired pyramid height: "))
+print(levels, id(levels))
+levels = int(input("Enter the desired pyramid height: "))
+print(levels)
+star_pyramid(levels)
+rstar_pyramid(levels)
+
+  
