04937035005f763b56b4056d379ce8f5fad33583
bdang2 <112588812+bdang12@users.noreply.github.com>
2022-10-19 23:35:18 +0000

    ch05/exercises:Scope and Accumulation

---
+++ ch05/exercises/scope.py
@@ -0,0 +1,25 @@
+#Question1
+def multiply(num):
+  result = 0
+  for i in range(num):
+    result = result + num
+  return result
+
+def main():
+  res = multiply(0)
+  print(res)
+  res = multiply(10)
+  print(res)
+main()
+#Question2
+def exponent(num,exp):
+  result=0
+  for i in range(exp):
+    result=result + num
+  return result
+def main():
+  res=exponent(2,5)
+  print(res)
+main()
+  
+#Question3
