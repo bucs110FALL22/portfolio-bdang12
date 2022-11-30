5f7793390a7bb634a85b0e6bb09ba238f2851fc8
bdang2 <112588812+bdang12@users.noreply.github.com>
2022-11-30 22:57:05 +0000

    ch09 exercises

---
+++ ch09/exercises/API.py
@@ -0,0 +1,13 @@
+{
+  "email": "support@apilayer.com",
+  "did_you_mean": "",
+  "user": "support",
+  "domain": "apilayer.net",
+  "format_valid": true,
+  "mx_found": true,
+  "smtp_check": true,
+  "catch_all": false,
+  "role": true,
+  "disposable": false,
+  "free": false,
+  "score": 0.8
